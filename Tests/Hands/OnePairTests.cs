0c1e423a6e6842e4070d27175bd72ee8c636c242
Stanislav Kozlov <llarean@ya.ru>
2025-10-25 14:26:29 +0300

    [AI] Add OnePair hand logic and unit tests
    Implemented the OnePair class to detect exactly one pair in a set of dice values.
    Added unit tests to verify correct behavior for valid and invalid hands.

---
+++ Tests/Hands/OnePairTests.cs
@@ -0,0 +1,29 @@
+﻿using PockerDice.Hands;
+
+namespace Tests;
+
+[TestFixture]
+public class OnePairTests
+{
+    [Test]
+    [TestCase(new[]{1, 1, 2, 3, 4})]
+    [TestCase(new[]{1, 2, 6, 6, 3})]
+    [TestCase(new[]{2, 1, 3, 5, 5})]
+    public void IsValid_HandContainsOnePair_IsTrue(int[] diceValues)
+    {
+        var onePair = new OnePair(diceValues);
+        
+        Assert.IsTrue(onePair.IsValid());
+    }
+    
+    [Test]
+    [TestCase(new[]{1, 2, 3, 4, 5})]
+    [TestCase(new[]{5, 4, 3, 1, 6})]
+    [TestCase(new[]{2, 4, 1, 3, 6})]
+    public void IsValid_HandNotContainsOnePair_IsFalse(int[] diceValues)
+    {
+        var onePair = new OnePair(diceValues);
+        
+        Assert.IsFalse(onePair.IsValid());
+    }
+}
---
+++ dice-pocker-console-eo/Hands/OnePair.cs
@@ -0,0 +1,64 @@
+﻿namespace PockerDice.Hands;
+
+/// <summary>
+/// Represents a poker hand that checks if a set of dice values contains exactly one pair
+/// <seealso cref="HandType.OnePair"/>
+/// </summary>
+/// <remarks>
+/// 3 + 3 + 6 + 5 + 2
+/// </remarks>
+public class OnePair
+{
+    private readonly int[] _diceValues;
+
+    /// <summary>
+    /// Represents a poker hand that verifies if an array of dice values contains exactly one pair
+    /// </summary>
+    public OnePair(int[] diceValues)
+    {
+        _diceValues = diceValues;
+    }
+
+    /// <summary>
+    /// Determines whether the current dice values represent a hand containing exactly one pair
+    /// </summary>
+    /// <returns>
+    /// A boolean value indicating whether the dice values contain exactly one pair
+    /// Returns true if there is exactly one pair, otherwise false
+    /// </returns>
+    public bool IsValid()
+    {
+        var diceDictionary = new Dictionary<int, int>();
+
+        foreach (var diceValue in _diceValues)
+        {
+            if (diceDictionary.ContainsKey(diceValue))
+            {
+                var diceNumber = diceDictionary[diceValue];
+                diceNumber++;
+                diceDictionary[diceValue] = diceNumber;
+            }
+            else
+            {
+                diceDictionary.Add(diceValue, 1);
+            }
+        }
+
+        var firstPair = false;
+        var secondPair = false;
+        
+        foreach (var dice in diceDictionary)
+        {
+            if (dice.Value == 2 && firstPair == false)
+            {
+                firstPair = true;
+            }
+            else if (dice.Value == 2)
+            {
+                secondPair = true;
+            }
+        }
+        
+        return firstPair && !secondPair;
+    }
+}
