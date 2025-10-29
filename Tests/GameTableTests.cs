610baf20ca47eacd0e463ae41b96097110d88c2e
Stanislav Kozlov <llarean@ya.ru>
2025-10-29 18:34:39 +0300

    [AI] Add basic GameTable constructor test
    Introduces a unit test to verify that the GameTable constructor initializes correctly with a given set of players.

---
+++ Tests/GameTableTests.cs
@@ -0,0 +1,22 @@
+﻿using PockerDice;
+
+namespace Tests;
+
+[TestFixture]
+public class GameTableTests
+{
+    [Test]
+    public void Constructor_ShouldInitializePlayers()
+    {
+        var random = new Random();
+        var dices = new Deck(random, 6).Dices();
+        
+        var player = new Player(dices);
+        var players = new Player[1];
+        players[0] = player;
+
+        var gameTable = new GameTable(players);
+
+        Assert.IsNotNull(gameTable);
+    }
+}
